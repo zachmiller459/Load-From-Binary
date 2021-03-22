d2f1cb6f9d7d80141b266253a532898e0324e11d
zachmiller459 <42632806+zachmiller459@users.noreply.github.com>
2021-03-21 21:05:36 -0700

    Add files via upload

---
+++ LoadFromBinary.h
@@ -0,0 +1,28 @@
+#ifndef LOADFROMBINARY
+#define LOADFROMBINARY
+
+#include <fstream>
+#include <cstdint>
+#include <iostream>
+
+template <typename T>
+T* LoadFromBinary(const char* filename)
+{
+    std::ifstream file(filename, std::ios::in|std::ios::binary|std::ios::ate);
+    T* Data;
+    if (file.is_open())
+    {
+        std::streampos Size = file.tellg();
+        int ItemCount = (int)Size/sizeof(T);
+        Data = new T[ItemCount];
+        file.seekg(0,std::ios::beg);
+        file.read((char*)Data, Size);
+        return Data;
+    }
+    else
+    {
+        std::cout << "Error opening file" << std::endl;
+        return nullptr;
+    }
+}
+#endif
